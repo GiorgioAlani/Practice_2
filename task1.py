e29c7e31383c05fe0d57809010825404745b6f08
GiorgioAlani <oritrmaxmen@gmail.com>
2025-10-08 09:09:12 +0300

    Исправлено первое задание

--- task1.py
+++ task1.py
@@ -1,6 +1,8 @@
 def speed():
     distance = int(input("Введите расстояние: "))
     time = int(input("Введите время: "))
-    velocity = distance/time
-    print(f"{velocity} км/ч")
+    if time!=0:
+        velocity = distance/time
+        print(f"{velocity} км/ч")
+    else:print("Ошибка: время равно нулю")
 speed()
