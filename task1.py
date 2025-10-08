e5e4294a40a28ea81a9a6b50f8861156d97e0705
GiorgioAlani <oritrmaxmen@gmail.com>
2025-10-08 13:45:32 +0300

    Удаление

--- task1.py
+++
@@ -1,8 +0,0 @@
-def speed():
-    distance = int(input("Введите расстояние: "))
-    time = int(input("Введите время: "))
-    if time!=0:
-        velocity = distance/time
-        print(f"{velocity} км/ч")
-    else:print("Ошибка: время равно нулю")
-speed()
