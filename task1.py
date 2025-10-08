637059797dac982cf0b172deab444c2d98681ddc
GiorgioAlani <oritrmaxmen@gmail.com>
2025-10-08 13:41:57 +0300

    Правки

--- task1.py
+++ task1.py
@@ -1,8 +1,8 @@
 def speed():
-    distance = int(input("Введите расстояние: "))
-    time = int(input("Введите время: "))
+    distance = int(input("Введите расстояние в метрах: "))
+    time = int(input("Введите время в часах: "))
     if time!=0:
         velocity = distance/time
-        print(f"{velocity} км/ч")
+        print(f"Вычисленная скорость: {velocity} км/ч")
     else:print("Ошибка: время равно нулю")
 speed()
