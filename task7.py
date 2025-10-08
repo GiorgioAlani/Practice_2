a95198f6cd88a8b076d6f04e4143ff028106a187
GiorgioAlani <oritrmaxmen@gmail.com>
2025-10-08 13:55:55 +0300

    Правки7

--- task7.py
+++ task7.py
@@ -2,5 +2,5 @@ def press():
     force = float(input("Введите силу в ньютонах: "))
     square = float(input("Введите площадь в квадратных метрах: "))
     press = force/square
-    print(f"{round(press, 2)} Па")
+    print(f"Вычисленное давление: {round(press, 2)} Па")
 press()
