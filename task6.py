c4e250413aa473e33697386c5dd765e0f7ada47b
GiorgioAlani <oritrmaxmen@gmail.com>
2025-10-08 13:54:40 +0300

    Правки6

--- task6.py
+++ task6.py
@@ -1,7 +1,7 @@
 def pot_energy():
-    mass = float(input("Введите массу: "))
-    height = float(input("Введите высоту: "))
+    mass = float(input("Введите массу в кг: "))
+    height = float(input("Введите высоту в метрах: "))
     acceleration = float(input("Введите ускорение свободного падения: "))
     pot_energy = mass * height * acceleration
-    print(f"{round(pot_energy,2)} Дж")
+    print(f"Потенциальная энергия: {round(pot_energy,2)} Дж")
 pot_energy()
